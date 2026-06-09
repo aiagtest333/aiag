2e4fd5c5d77c6a036c680d4b7e4e9f46bc0d7327
aiagtest333 <aiagtest@outlook.com>
2026-06-09 14:26:23 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,8 @@
 package com.demo;
 
 public class App {
-    static final String AWS_KEY = "AKIAIOSFODNN7EXAMPLEZ";
-    static final String AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPXRfiCYEXAMPLEKEY";
+    static final String DB_PASSWORD = "Kx9#mP2$vL8@nQ4&wR7!jT5^";
+    static final String API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.dozjgNryP4J3jVmNHl0w5N_XgL0n3I9PlFUP0THsR8U";
 
     public static void main(String[] args) {
         System.out.println("DevSecOps Pipeline Demo");
