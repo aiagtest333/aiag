a5990bd7bbd75f29860f385545e31701c5a81d4e
aiagtest333 <aiagtest@outlook.com>
2026-06-09 16:00:39 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,6 @@ package com.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("DevSecOps Pipeline Demo");
-        String s = "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";
+        String s = "https://hooks.slack.com/services/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";
     }
 }
